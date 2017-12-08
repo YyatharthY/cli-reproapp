cea6cdeb0747838e20ddad2b996e8c1150360977
sataqi <syedalitaqiabbas@gmail.com>
2017-12-08 19:35:01 +0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { isInDevMode } from './app.constants';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title: string;
+  
+  ngOnInit() {
+    this.title = isInDevMode ? 'App in dev mode works.' : 'App is prod mode works.'
+  }
+}
---
+++ src/app/app.constants.ts
@@ -0,0 +1,3 @@
+import { isDevMode } from '@angular/core';
+
+export const isInDevMode = isDevMode();
